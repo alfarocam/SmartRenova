3ad1106a30c17286076ff3f16737d0ad8b784c3f
Camila Alfaro <camilaalfa20@gmail.com>
2025-07-30 17:56:36 -0600

    AVANCE III Update

--- src/main/java/com/smartrenova/controller/ProductoController.java
+++ src/main/java/com/smartrenova/controller/ProductoController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+//
 @Controller
 @RequestMapping("/producto")
 public class ProductoController {
